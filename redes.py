00b8466a28ee3871501743b52c966639d0369276
Raquel <raquelmartiniano14@gmail.com>
2025-10-22 11:24:52 +0000

    corrigindo

--- redes.py
+++ redes.py
@@ -1,30 +1,44 @@
-class IPAddress: #IMCOMPLETO
+class IPAddress:
     def __init__(self, Ipv4:str, mascara:str):
         self.Ipv4 = Ipv4
         self.mascara = mascara
 
     def converter_bit(self):
         #para converter o string em inteiro
+        pass
 
     def set_ip(self, Ipv4):
-        self.ip_bit = list(map(int, self.Ipv4.split('.')))        
+        self.ip_bit = list(map(int, self.Ipv4.split('.')))      
         if self.Ipv4 < 0 or self.Ipv4 > 255:
             raise ValueError("Erro: IP inválido")
         else:
             self.Ipv4 = Ipv4
 
     def set_mascara(self, mascara):
-        self.converter_bit() #analisar sobre essa parte
+        self.mask_bit = list(map(int, self.mascara.split('.'))) 
         if self.mascara < 0 or self.mascara > 255:
             raise ValueError("Erro: Máscara inválida")
         else:
             self.mascara = mascara
 
-    def calc_bit(self):
-        self.converter_bit()
-        self.rede_bit = [self.ip_bit[i] & self.mask_bit[i] for i in range(4)] #analisar sobre essa parte
-        self.rede = '.'.join(map(str, self.rede_bit))
-        return self.rede
+    def conversor_bitip(self):
+        self.binario_ip = []
+        for octeto in self.ip_bit:
+            self.binario_ip.append(bin(octeto))
+
+        for b in self.binario_ip:
+            self.verdadeiromask = format(binario_ip, '08b')
+        return self.verdadeiromask
+
+    def conversor_bitmask(self):
+        self.verdadeiromask = []
+        for octeto in self.mask_bit:
+            self.binario_mask.append(bin(octeto))
+        
+        for b in self.verdadeiromask:        
+            self.verdadeiromask = format(binario_mask, '08b')
+        return self.verdadeiromask
+
 
     def get_ip(self):
         return self.Ipv4
@@ -32,19 +46,14 @@ class IPAddress: #IMCOMPLETO
     def get_mascara(self):
         return self.mascara
 
-
     def pertence_a_rede(self, IP):
         self.endereco = str(print(input()))
-
 
         if self.endereco == self.Ipv4: #analisar sobre essa parte
             return True
         return False
 
-    def __str__(self):#analisar sobre essa parte
+    def __str__(self): # analisar sobre essa parte
         return f""
 
 ip = IPAddress()
-
-
-
